a082101191367b86fad2d1a06fbbbf9892e58dc0
João Pedro Pereira <jplpereira@gmail.com>
2016-11-27 09:27:52 -0200

    Retirando o símbolo de comentário das ações do script

--- TemRemedioAi.sh
+++ TemRemedioAi.sh
@@ -15,29 +15,29 @@ PROJECT_REPO="https://github.com/jplpereira/GCS-TemRemedioAi.git"
 echo -e "${YELLOW}#################################################################################################################"
 echo "Iniciando a instalação do projeto TemRemedioAi"
 echo "Adicionando repositórios"
-#sudo add-apt-repository ppa:webupd8team/java
-#sudo add-apt-repository ppa:paolorotolo/android-studio
-#sudo apt-get update
+sudo add-apt-repository ppa:webupd8team/java
+sudo add-apt-repository ppa:paolorotolo/android-studio
+sudo apt-get update
 echo "Instalando o Java Development Kit"
-#sudo apt-get install oracle-java8-installer
-#sudo apt-get install oracle-java8-set-default
+sudo apt-get install oracle-java8-installer
+sudo apt-get install oracle-java8-set-default
 echo "Instalando o Android Studio"
-#sudo apt-get install android-studio
+sudo apt-get install android-studio
 #Instalando bibliotecas adicionais caso a versão do sistema seja de 64 bits
 if [ "${VERSION}" == "${TARGET_VERSION}" ]; then
 echo "Seu sistema é de 64 bits. Será necessário instalar algumas bibliotecas"
 echo "Instalando bibliotecas"
-#sudo apt-get install lib32z1 lib32ncurses5 lib32bz2-1.0 lib32stdc++6
+sudo apt-get install lib32z1 lib32ncurses5 lib32bz2-1.0 lib32stdc++6
 else
 echo "Seu sistema é de 32 bits. Não será necessário instalar outras bibliotecas"
 fi
 echo "Criando a pasta de projetos do Android Studio"
-#sudo mkdir $PROJECTS_FOLDER
+sudo mkdir $PROJECTS_FOLDER
 echo "Clonando o projeto do GitHub"
-#cd $PROJECTS_FOLDER
-#sudo git clone $PROJECT_REPO
+cd $PROJECTS_FOLDER
+sudo git clone $PROJECT_REPO
 echo "Criando atalho para o desktop"
 echo -e "[Desktop Entry]\nVersion=1.0\nType=Application\nName=Android Studio\nExec='/opt/android-studio/bin/studio.sh' %f\nIcon=/opt/android-studio/bin/studio.png\nCategories=Development;IDE;\nTerminal=false\nStartupNotify=true\nStartupWMClass=android-studio" >> ~/.local/share/applications/androidstudio.desktop
 echo "Inicializando o Android Studio"
-#./opt/android-studio/bin/studio.sh
+./opt/android-studio/bin/studio.sh
 echo -e "#################################################################################################################${NOCOLOR}"
